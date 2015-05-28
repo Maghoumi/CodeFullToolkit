c34b9c654a14ebb38447a6de6b18928ea85cf42e
Mehran Maghoumi <maghoumi@gmail.com>
2015-05-28 11:25:00 -0400

    Separate files for VBO and MeshVertex structs

---
+++ CodeFullToolkit/Graphics/VBO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CodeFull.Graphics
+{
+    /// <summary>
+    /// Represents the required VBO handles
+    /// </summary>
+    public struct Vbo
+    {
+        /// <summary>
+        /// The vertex buffer handle
+        /// </summary>
+        public int vertexId;
+
+        /// <summary>
+        /// The color buffer handle
+        /// </summary>
+        public int colorId;
+
+        /// <summary>
+        /// The color selection buffer handle
+        /// </summary>
+        public int selectColorId;
+
+        /// <summary>
+        /// The face arrays handle
+        /// </summary>
+        public int faceId;
+
+        /// <summary>
+        /// Number of elements in this VBO
+        /// </summary>
+        public int numElements;
+    }
+}
